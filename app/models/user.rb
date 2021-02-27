0232c2417308edc3bf6479bbe4e554ce08237ca1
Danil Pismenny <danil@brandymint.ru>
2021-02-27 15:14:36 +0300

    Add User#full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
 
   has_many :memberships
   has_many :study_rooms, through: :memberships
+
+  before_validation do
+    self.phone = Phonelib.parse(phone).to_s
+  end
+
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :phone, presence: true, phone: true
+
+  # ФИО
+  def full_name
+    [surname, firstname, middlename].compact.join(' ')
+  end
+
+  # ФИО
+  def full_name=(value)
+    self.surname, self.firstname, self.middlename = value.chomp.squish.split(/\s/)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "parse full_name" do
+    full_name = 'Иванова Мария Ивановна'
+    user = User.new
+    user.full_name = full_name
+    assert_equal user.firstname, 'Мария'
+    assert_equal user.surname, 'Иванова'
+    assert_equal user.middlename, 'Ивановна'
+    assert_equal user.full_name, full_name
+  end
 end
