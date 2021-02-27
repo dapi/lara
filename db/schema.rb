fc7a02d7033e16e6dac3643eb8bb7ef1fe329d3b
Danil Pismenny <danil@brandymint.ru>
2021-02-27 15:16:03 +0300

    Add User#telegram_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_205820) do
+ActiveRecord::Schema.define(version: 2021_02_27_121544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_02_26_205820) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "telegram_id"
   end
 
   add_foreign_key "memberships", "study_rooms"
