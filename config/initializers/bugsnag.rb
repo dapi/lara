f8a0ca2cd0b6a0f1ba7bde631d47cae871425070
Danil Pismenny <danil@brandymint.ru>
2021-02-28 19:04:47 +0300

    Move bugsnag api_key to credentials

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,11 @@
-Bugsnag.configure do |config|
-  config.api_key = "eee16e119134b568676901929efd056e"
-  # config.notify_release_stages = %w(production staging)
-  config.send_code = true
-  config.send_environment = true
+
+if Rails.application.credentials.bugsnag_api_key.present?
+  Bugsnag.configure do |config|
+    config.api_key = Rails.application.credentials.bugsnag_api_key
+    # config.notify_release_stages = %w(production staging)
+    config.send_code = true
+    config.send_environment = true
+  end
+else
+  Rails.logger.warn 'Bugsnag is not configured. Setup bugsnag_api_key in credentials'
 end
