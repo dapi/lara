effd5e9734d4264783a3841ff5e90d85a53ead2b
Danil Pismenny <danil@brandymint.ru>
2021-02-28 20:04:16 +0300

    Добавил /welcome message

--- app/controllers/concerns/action_invite.rb
+++ app/controllers/concerns/action_invite.rb
@@ -12,28 +12,4 @@ module ActionInvite
       reply_with :message, text: 'Укажите роль (student, parents, teacher) и ФИО. Например: /invite student Письменная Агата'
     end
   end
-
-  private
-
-  def accept_invite(invite)
-    invite.with_lock do
-      user = User
-        .create!(full_name: invite.full_name, telegram_id: from['id'], telegram_info: from)
-      @current_user = user
-      case invite.role
-      when 'parents'
-      when 'student'
-        invite.study_room.student_users << user
-      when 'teacher'
-        invite.study_room.teacher_users << user
-      else
-        raise "Unknown invite role #{invite.role} for #{invite.id}"
-      end
-
-      respond_with :message,
-        text: multiline("Привет, #{user.name}!",
-                        "Я Лара - личный помощник по учебной части. Теперь я знаю что ты #{Invite.human_enum_name :role, invite.role} в #{invite.study_room.title}")
-      invite.destroy!
-    end
-  end
 end
--- app/controllers/concerns/action_start.rb
+++ app/controllers/concerns/action_start.rb
@@ -2,7 +2,7 @@ module ActionStart
   def start!(message = '', *args)
     if logged_in?
       respond_with :message,
-        text: "#{current_user.firstname}, привет!"
+        text: multiline("#{current_user.firstname}, привет!", Settings.welcome_message.each_line.to_a)
     elsif message.gsub!(/^i_/,'')
       invite = Invite.find_by(key: message)
       if invite.present?
@@ -16,4 +16,27 @@ module ActionStart
     end
   end
 
+  private
+
+  def accept_invite(invite)
+    invite.with_lock do
+      user = User
+        .create!(full_name: invite.full_name, telegram_id: from['id'], telegram_info: from)
+      @current_user = user
+      case invite.role
+      when 'parents'
+      when 'student'
+        invite.study_room.student_users << user
+      when 'teacher'
+        invite.study_room.teacher_users << user
+      else
+        raise "Unknown invite role #{invite.role} for #{invite.id}"
+      end
+
+      respond_with :message,
+        text: multiline("Привет, #{user.name}!",
+                        "Я Лара - личный помощник по учебной части. Теперь я знаю что ты #{Invite.human_enum_name :role, invite.role} в #{invite.study_room.title}")
+      invite.destroy!
+    end
+  end
 end
