3e1b805d362adf0f0e8c519b66e479f0919b45dc
Danil Pismenny <danil@brandymint.ru>
2021-02-28 19:49:34 +0300

    m

--- app/controllers/concerns/action_class.rb
+++ app/controllers/concerns/action_class.rb
@@ -3,7 +3,7 @@ module ActionClass
   def class!
     reply_with :message,
       text: multiline(study_room.title, nil,
-                      'Ученики: ' + study_room.student_users.map(&:full_name).join(', '),
+                      'Учитиля: ' + study_room.student_users.map(&:full_name).join(', '),
                       'Учетиля: ' + study_room.teacher_users.map(&:full_name).join(', '),
                       'Родители: ' + study_room.parents.map(&:full_name).join(', '),
                       'У всего класса: ' + humanized_stars(study_room.total_stars)
