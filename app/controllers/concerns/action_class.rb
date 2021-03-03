19789b9ed5f97a02570a46f1009ab483fc7b579a
Danil Pismenny <danil@brandymint.ru>
2021-03-03 10:02:14 +0300

    Respect group chats

--- app/controllers/concerns/action_class.rb
+++ app/controllers/concerns/action_class.rb
@@ -1,9 +1,12 @@
 module ActionClass
   # Информация о классе. Общее количество звёзд
-  def class!
+  def class!(*args)
     reply_with :message,
       text: multiline(study_room.title, nil,
-                      'У всего класса: ' + humanized_stars(study_room.total_stars)
-                     )
+                      *study_room.students.includes(:wallet).map { |s| "#{s.name}: " + humanized_stars(s.wallet.stars) },
+                      nil,
+                      '**У всего класса: ' + humanized_stars(study_room.total_stars) + '**'
+                     ),
+       parse_mode: :Markdown
   end
 end
